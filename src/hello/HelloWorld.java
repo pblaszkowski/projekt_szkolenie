19146fdc018ad318097ec6a63d5239e835fd6711
Piotr Błaszkowski <piotrblaszk@gmail.com>
2017-05-21 14:01:23 +0300

    Poprawka

--- src/hello/HelloWorld.java
+++ src/hello/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		System.out.println("wszystko pod kontrolą");
 		System.out.println("Bla bla bla bal");
 
 	}
