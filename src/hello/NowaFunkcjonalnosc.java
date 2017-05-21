970a49757ffefd81100d42ed8cb1fe48387edd2d
Piotr Błaszkowski <piotrblaszk@gmail.com>
2017-05-21 13:56:46 +0300

    Nowa funkcjonalność wersja 1.0

---
+++ src/hello/NowaFunkcjonalnosc.java
@@ -0,0 +1,12 @@
+package hello;
+
+public class NowaFunkcjonalnosc {
+
+	public static void main(String[] args) {
+		System.out.println("Nowa super fajna rzecz w nowej klasie");
+		
+		
+		System.out.println("Ale wymaga dopracowania");
+	}
+
+}
